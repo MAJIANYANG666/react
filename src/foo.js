1c69cfd79ae78f38f80c387e40def50c0c086815
马建阳 <1297606787@qq.com>
2017-12-26 18:18:30 +0800

    添加foo.js

---
+++ src/foo.js
@@ -0,0 +1,3 @@
+export default function(){
+    return '芳的教程'
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-// import _ from 'lodash';
-// import j from 'jquery';
-//
-// function component() {
-//     // var element = document.createElement('div');
-//     var element = j('<div></div>');
-//     // Lodash, currently included via a script, is required for this line to work
-//     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.html(_.join(['Hello','webpack'], ' '))
-//
-//     // return element;
-//     return element.get(0);
-// }
-//
-// document.body.appendChild(component());
-console.log(1)
+//import from 'lodash' 的意思是从 lodash 里得到默认导出，并将默认导出命名为 ，这个 _ 可以换成任何一个其他的变量名。
+import _ from 'lodash';
+import j from 'jquery';
+import foo from './foo'
+function component() {
+    // var element = document.createElement('div');
+    var element = j('<div></div>');
+    // Lodash, currently included via a script, is required for this line to work
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.html(_.join(['Hello','webpack'], ' '))
+
+    // return element;
+    return element.get(0);
+}
+
+document.body.appendChild(component());
+console.log(foo)
+console.log(foo())

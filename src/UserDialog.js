15046577e7318eac685943c707b173b415725432
马建阳 <1297606787@qq.com>
2018-01-15 20:09:50 +0800

    切换UserDialog.css

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,13 +1,24 @@
 import React,{Component} from 'react';
-
+import './UserDialog.css'
 export default class UserDialog extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            selected:'signUp'
+        }
+    }
+    switch(e){
+        this.setState({
+            selected:e.target.value
+        })
+    }
     render(){
         return (
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
-                    <nav>
-                        <input type="radio"/>注册
-                        <input type="radio"/>登陆
+                    <nav onChange={this.switch.bind(this)}>
+                        <label><input type="radio" value="signUp" checked={this.state.selected==='signUp'}/>注册</label>
+                        <label><input type="radio" value="signIn" checked={this.state.selected==='signIn'}/>登陆</label>
                     </nav>
                     <div className="panes">
                         <form className="signUp">
@@ -15,6 +26,7 @@ export default class UserDialog extends Component{
                                 <label>用户名</label>
                                 <input type="text"/>
                             </div>
+
                             <div className="row">
                                 <label>密码</label>
                                 <input type="password"/>

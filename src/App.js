179abf6a97ce1f701f3221cb9bc49a64f3e6bb76
马建阳 <1297606787@qq.com>
2018-01-15 15:29:02 +0800

    install lencloud

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import 'normalize.css'
 import * as localStore from './localStore'
-
 //按 es6 的规范 import * as obj from "xxx" 会将 "xxx" 中所有 export 导出的内容组合成一个对象返回
 class App extends Component {
   constructor(props){

3bf4e5b0408476b12c78fe7f84dc76b25aa95f16
马建阳 <1297606787@qq.com>
2018-01-15 15:56:39 +0800

    新建leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,13 @@
+import AV from 'leancloud-storage'
+var APP_ID = 'kJVcuD8Fm4ufCYsQdzlvFQ4W-gzGzoHsz';
+var APP_KEY = 'fJgDDI92gXvaGIdOTK4qByct';
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY,
+
+    // 启用美国节点
+    region: 'us'
+
+
+})S
+export default AV;

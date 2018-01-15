ba11f13b86e3653855739dd9574a9380d42c7fed
马建阳 <1297606787@qq.com>
2018-01-15 15:38:17 +0800

    测试leancloud功能

--- src/App.js
+++ src/App.js
@@ -13,7 +13,13 @@ AV.init({
     appId: APP_ID,
     appKey: APP_KEY
 });
-
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+    words: 'Hello World!'
+}).then(function(object) {
+    alert('LeanCloud Rocks!');
+})
 
 class App extends Component {
   constructor(props){

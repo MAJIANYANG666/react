9b07bb2821c838f4478424c7a65487ef07d30e46
马建阳 <1297606787@qq.com>
2018-01-23 14:35:28 +0800

    仿照注册，完成登陆功能

--- src/App.js
+++ src/App.js
@@ -46,7 +46,11 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-          {this.state.user.id?null:<UserDialog onSignUp={this.onSignUp.bind(this)}/>}
+          {this.state.user.id?
+              null:
+              <UserDialog
+                  onSignUp={this.onSignUp.bind(this)}
+                  onSignIn={this.onSignIn.bind(this)}/>}
       </div>
     );
   }
@@ -59,6 +63,11 @@ class App extends Component {
   onSignUp(user){
       let stateCopy = JSON.parse(JSON.stringify(this.state))
       stateCopy.user = user
+      this.setState(stateCopy)
+  }
+  onSignIn(user){
+      let stateCopy=JSON.parse(JSON.stringify(this.state))
+      stateCopy.user=user
       this.setState(stateCopy)
   }
   componentDidUpdate(){
